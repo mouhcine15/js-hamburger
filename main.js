7eed3091adee18b1b1b8b9b9929b5f14177b413f
mouhcine <mouhcine15.00@gmail.com>
2022-05-06 17:12:55 +0200

    aggiunta precisazione

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const btn_open = document.querySelector(".fa-bars");
-const btn_close = document.querySelector(".fa-items");
+const btn_open = document.querySelector(".header-right .fa-bars");
+const btn_close = document.querySelector(".hamburger-menu .close .fa-times");
 const hamburger = document.querySelector(".hamburger-menu");
 
 btn_open.addEventListener('click',
