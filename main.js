36abb7346515b733ffe6267e2729305e487cb8d4
mouhcine <mouhcine15.00@gmail.com>
2022-05-06 16:20:04 +0200

    3 costanti

---
+++ main.js
@@ -0,0 +1,11 @@
+const btnOpen = document.getElementsByClassName("fa-bars");
+const btnClose = document.getElementsByClassName("fa-items");
+const hamburgerMenu = document.getElementsByClassName("hamburger-menu");
+
+element.addEventListener('click',
+    
+function toggleText() {
+    
+    
+}
+);
